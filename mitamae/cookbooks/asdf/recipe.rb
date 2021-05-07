46429b4d268ede30d92d4598e6d5c37e45450b2d
lime1024 <lime1024@gmail.com>
2021-05-07 23:20:12 +0900

    path が通っていないから

--- mitamae/cookbooks/asdf/recipe.rb
+++ mitamae/cookbooks/asdf/recipe.rb
@@ -6,7 +6,7 @@ dotfile '.asdfrc'
 
 plugins = File.open(File.join(node[:dotfiles], 'asdf-plugins'))
 plugins.each do |plugin|
-  execute "asdf plugin-add #{plugin}" do
-    not_if "asdf plugin list | grep #{plugin}"
+  execute "#{node[:home]}/.asdf/bin/asdf plugin-add #{plugin}" do
+    not_if "#{node[:home]}/.asdf/bin/asdf plugin list | grep #{plugin}"
   end
 end
