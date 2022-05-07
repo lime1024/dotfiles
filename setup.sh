1e0ada83eb027f804501b48c2be5aa47ae56e308
lime1024 <lime1024@gmail.com>
2022-05-07 16:39:00 +0900

    arm64 でも動くようにしたいから

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@ set -o errexit -o pipefail -o xtrace
 # install homebrew
 eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# check architecture
+case "$(uname -m)" in
+  x86_64) arch=x86_64 ;;
+  arm64) arch=aarch64 ;;
+  *) echo "unknown arch" >&2; exit 1
+esac
+
 # pull mitamae
-curl -fsSLo /usr/local/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/latest/download/mitamae-x86_64-darwin
+curl -fsSLo /usr/local/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/latest/download/mitamae-${arch}-darwin
 chmod +x /usr/local/bin/mitamae
